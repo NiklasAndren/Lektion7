df437a0eb4e7b08cc07184888ab595e4b970a4fd
Niklas Andren <niklas.andren@hotmail.com>
2012-09-03 16:48:07 +0200

    Woop

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lektion7.Models.Repositories;
+using Lektion7.Models.Entities;
+using Lektion7.ViewModels;
+
+namespace Lektion7.Controllers
+{
+    public class PostsController : Controller
+    {
+        //
+        // GET: /Posts/
+
+        public ActionResult Index(int id)
+        {
+            var post = Repository.Instance.GetPostByIndex(id);
+
+            var MyViewModel = new MyViewModel();
+            MyViewModel.Post = post;
+            MyViewModel.MyInteger = 5;
+
+            return View(MyViewModel);
+        }
+
+    }
+}
